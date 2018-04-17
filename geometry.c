065932ad69fed471100874917854abe8ef6e1726
user <keea2562@colorado.edu>
2018-04-16 18:30:29 -0600

    kevin

--- geometry.c
+++ geometry.c
@@ -1,8 +1,8 @@
 /*
  * geometry.c
- * Andy Sayler
+ * Kevin Eastman
  * CSCI 3308
- * Summer 2014
+ * Spring 2018
  *
  * This file contains a simple geomtery functions.
  *
@@ -68,4 +68,15 @@ void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b
     mid->x = ((a->x + b->x) / 2.0 );
     mid->y = ((a->y + b->y) / 2.0 );
 
+}
+
+double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const coord_2d_t* c){
+    double area = a->x * (b->y - c->y);
+    area += b->x * (c->y - a->y);
+    area += c->x * (a->y - b->y);
+    area = area / 2;
+    if (area < 0){
+        area = -area;
+    }
+    return area;
 }
